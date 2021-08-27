df37acd294f11a38758e26cae27063d5521a02e6
Austin Fifield <afifield@uncc.edu>
2021-08-27 19:44:25 -0400

    Corrected distance equation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.sensors.Limelight;
 
 public class Robot extends TimedRobot {
   private Command autonomousCommand;
@@ -89,7 +88,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    SmartDashboard.putNumber("Robot-Tower Distance", robotContainer.limelight.getDistance());
+    SmartDashboard.putNumber("Robot-Tower Distance", robotContainer.limelight.getBumperDistance());
   }
 
   @Override
